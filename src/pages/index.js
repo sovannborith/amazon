66fd8b376b52a069891c9d9443b4538c6763999f
Sovannborith Yun <yun.sovannborith@gmail.com>
2022-11-03 16:04:31 +0700

    Add session in index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,7 +4,7 @@ import Banner from "../components/Banner";
 import Header from "../components/Header";
 import ProductFeed from "../components/ProductFeed";
 
-export default function Home({ products }) {
+export default function Home({ products, session }) {
   return (
     <div>
       <Head>
@@ -23,8 +23,8 @@ export default function Home({ products }) {
 
 export async function getServerSideProps(context) {
   const session = await getSession(context);
-  const products = await fetch("https://fakestoreapi.com/products").then(
-    (res) => res.json()
-  );
+  const products = await fetch("https://fakestoreapi.com/products")
+    .then((res) => res.json())
+    .catch((err) => console.log(err));
   return { props: { products, session } };
 }
